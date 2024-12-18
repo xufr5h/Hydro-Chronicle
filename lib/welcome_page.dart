74b1c9e48f49665b6a6f8ec743403fa0bf384f8e
Lija Maharjan <lijamaharjan369@gmail.com>
2024-12-18 14:08:07 +0545

    Completed the welcome page after routing the button

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hydro_chronicle/components/welcome_row.dart';
+import 'package:hydro_chronicle/home_page.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -16,7 +17,7 @@ class WelcomePage extends StatelessWidget {
             color: Color.fromARGB(255, 177, 218, 245),
           ),
         ),
-        const Align(
+        Align(
           alignment: Alignment.topCenter,
           child: Padding(
             padding: EdgeInsets.only(top: 70),
@@ -24,7 +25,7 @@ class WelcomePage extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Row(
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
@@ -42,7 +43,7 @@ class WelcomePage extends StatelessWidget {
                     ),
                   ],
                 ),
-                Text(
+                const Text(
                   'Hydro Chronicle',
                   style: TextStyle(
                     fontSize: 30,
@@ -51,30 +52,77 @@ class WelcomePage extends StatelessWidget {
                   ),
                 ),
                 // Row 1
-                SizedBox(height: 100),
-                WelcomeRow(
+                const SizedBox(height: 100),
+                const WelcomeRow(
                     title: 'Create Tasks Easily and Quickly',
                     description: 'Input tasls, subtasks and repetitive tasks',
                     imagePath: 'assets/images/createImage.png'),
                 // Row 2
-                SizedBox(height: 30),
-                WelcomeRow(
+                const SizedBox(height: 30),
+                const WelcomeRow(
                     title: 'Task Reminders',
                     description:
                         'Set Reminders and never miss important things',
                     imagePath: 'assets/images/taskReminder.png'),
                 // Row 3
-                SizedBox(height: 30),
-                WelcomeRow(
+                const SizedBox(height: 30),
+                const WelcomeRow(
                     title: 'Personalized Widgets',
                     description:
                         'Create widgets and view your tasks more easily.',
-                    imagePath: 'assets/images/createWidgets.png')
+                    imagePath: 'assets/images/createWidgets.png'),
+                // whole button
+                const SizedBox(height: 100),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 50),
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        const Image(
+                          image: AssetImage('assets/images/furContinue.png'),
+                          height: 160,
+                          width: 160,
+                        ),
+                        Positioned(
+                          right: 0,
+                          left: 0,
+                          bottom: -25,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const HomePage()));
+                            },
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(200, 60),
+                              backgroundColor:
+                                  const Color.fromARGB(255, 67, 92, 218),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              elevation: 5,
+                            ),
+                            child: const Text(
+                              'Continue >>',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontFamily: 'Itim',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
         ),
-        // Row 1
       ],
     ));
   }
